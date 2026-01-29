bf02b07170e637e9a279a284556fa3e3e64d8760
jhosikas <jhosikas@gmail.com>
2026-01-29 14:29:57 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,11 @@
+public class Datatype{
+    public static void main(String[] args){
+        int i=10;
+        float f=19.19f;
+        double d=19.1900000;
+        char c='j';
+        String s="Jhosika";
+        boolean b=false;
+        System.out.println(i+f+d+c+s+b);
+    }
+}
